73a7c06b198067d4bd21a5f92f1d99ab2d1f72a8
Barbie <barbie@missbarbell.co.uk>
2015-12-06 22:35:12 +0000

    upped version

--- lib/Bot/BasicBot/Pluggable/Module/Notify.pm
+++ lib/Bot/BasicBot/Pluggable/Module/Notify.pm
@@ -3,7 +3,7 @@ package Bot::BasicBot::Pluggable::Module::Notify;
 use warnings;
 use strict;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 #----------------------------------------------------------------------------
 
